0fcc2e53b46c0e0599c1ddccdad6983d758d2c87
zantex94 <reneseer@gmail.com>
2021-09-19 23:38:06 +0200

    made CSS,Hmtl and settet up some javascript. need multiplayer and leaderboard to finish

--- javascript/gættal.js
+++ javascript/gættal.js
@@ -1,67 +1,121 @@
-var randomNumber = Math.floor(Math.random() * 1000 + 1);
+
 /* this function starts a game */
-function newGame(){
-    let sumvalues = 10;
-    let date = new Date()
-    var howMany = Number(prompt('How many players?'));
-    var playerName = String(prompt('Players Name'));
-    
-    console.log(howMany);
-    console.log(playerName);
+let randomNumber = 0;
+let sumvalues = 10;
+let person = [];
+let playerTurn = 0;
+let time = 0;
+let hours = 0;
+let min =  0;
+let sec =  0;
+let a = "0";
+let b = "0";
+let c = "0";
   
-}
+class Game{
 
 
-/* a function that checks the value of a player */
-function checkValue(){
-    console.log("Check value: " + randomNumber);
-    let userValue = $('inputNumber').value;
-    let result = $('resultInput');
-    let scoreValue = $('scoreValue');
+    Person = {
+        name: "",
+        randomNumber: 0,
+        sumvalues: 0,
+        // date = 0
+    };
+    
+  
+    newGame(){
+    randomNumber = Math.floor(Math.random() * 1000 + 1);  
+    // var howMany = Number(prompt('How many players?'));
+    var playerName = String(prompt('Players Name'));
+    this.Person.name = playerName;
+    let playersName = $('playerName');
     let timePlayed = $('timePlayed');
-    console.log("random number is: " + randomNumber);
-    console.log("uservalue is: " + userValue);
-
-
-    console.log(sumvalues);
-
-    if(sumvalues === 0){
-        result.innerHTML = "øv du fik ikke gættet rigtigt. Det endelige resultat er:  " + randomNumber;
-
+    playersName.innerHTML = "" + this.Person.name;
+    this.Person.sumvalues = 10;
+   
+ time =  setInterval(function(){ 
+    timePlayed.innerHTML = a + hours + ":" + b + min + ":" + c + sec;
+     sec++
+     if(sec === 10){
+         c = "";
+     }
+     if(min === 10){
+        b = "";
+    }
+    if(hours === 10){
+        a = "";
     }
 
+     if(sec === 59){
+         sec = 0;
+         c = "0";
+         min++;
+     }
+     if(min === 59){
+         min = 0;
+         b = "0";
+         hours++;
+     }
+
+}, 1000);
+   
+}
+
+/* a function that checks the value of a player */
+    checkValue(){
+     let userValue = $('inputNumber').value -0;
+     /* display the result */
+     let result = $('resultInput');
+     /* display the score */
+     let scoreValue = $('scoreValue');
+    /* display time */
+     let timePlayed = $('timePlayed');
+
+    
 
 /* The if statement checks the value of a giving number from the user */
     if(userValue === randomNumber){
         /* there is a winnder*/
         result.innerHTML = "Tillykke du gættede rigtigt ";
+        scoreValue.innerHTML = " " + sumvalues;
+        clearInterval(time);
+        console.log(timePlayed.innerHTML);
 
     }else if (userValue < randomNumber){
         /* the value is higher than the genereated number */
-        result.innerHTML = "Tallet er højre end ";
-        sumvalues -= sumvalues;
+        result.innerHTML = "Tallet er højre ";
+        sumvalues--;
+        scoreValue.innerHTML = "" + sumvalues;
 
 
     }else{
         /* the value is less than the genereated number */
-        result.innerHTML = "Tallet er lavere end ";
-        sumvalues -= sumvalues;
+        result.innerHTML = "Tallet er lavere ";
+        sumvalues--;
+        scoreValue.innerHTML = "" + sumvalues;
   
     }
+    if(sumvalues === 0){
+        result.innerHTML = "Game over! Det rigtige result   at er:  " + randomNumber;
+        clearInterval(time);
+        console.log(timePlayed.innerHTML);
+    }
 
+}
 }
 
 
 function doSomething () {
     let buttonFormNewGame = $("newGame");
     let buttonFormSubmit = $("submitgæt");
+    let b = new Game();
     buttonFormNewGame.addEventListener('click', function(event){
         event.preventDefault()
-        newGame();
+        b.newGame();
       });
       buttonFormSubmit.addEventListener('click', function(event){
         event.preventDefault()
-        checkValue();
+        b.checkValue();
       });
 }
 window.addEventListener('load', doSomething);
