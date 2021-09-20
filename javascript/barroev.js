65394f8918ce3d497996700d306037f384e6e211
Kenneth <kenneth@kennt.dk>
2021-09-20 13:20:39 +0200

    created seperate createboard.js

--- javascript/barroev.js
+++ javascript/barroev.js
@@ -51,28 +51,8 @@ function loadGame (e){
         dices: dices,
         players: playersArr
     }
-    
-    // create board
-    let board = document.createElement('section');
-    board.setAttribute('id', 'board');
-    let diceContainer = document.createElement('div');
-    diceContainer.setAttribute('class', 'diceContainer');
-    let playerContainer = document.createElement('div');
-    playerContainer.setAttribute('class', 'playerContainer');
-    let playerbox = document.createElement('div');
-    playerbox.setAttribute('class', 'playerbox');
-    let scoreboard = document.createElement('div');
-    scoreboard.setAttribute('class', 'score');
-    let die = document.createElement('div');
-    die.setAttribute('class', 'dice');
-    
-    // append childs
-    page.appendChild(board);
-    board.append(diceContainer, playerContainer); 
-    diceContainer.appendChild(die);
-    playerContainer.append(playerbox, scoreboard);
 
-
+    createBoard();
     console.log(gameSettings);
 }
 
