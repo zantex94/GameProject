1a5c736fff7f4fba033149b0f7316b23901b70c0
zantex94 <reneseer@gmail.com>
2021-09-23 13:08:36 +0200

    Ready to merge

--- javascript/player.js
+++ javascript/player.js
@@ -2,12 +2,12 @@
 
 class Player{
     /**construct a player */
-    constructor(name, randomNumber, sumValues, timeUsed, Guessright, done){
+    constructor(name, randomNumber, sumValues, timeUsed, guessright, done){
         this.name = name;
         this.randomNumber = randomNumber;
         this.sumValues = sumValues;
         this.timeUsed = timeUsed;
-        this.Guessright = Guessright;
+        this.guessright = guessright;
         this.done = done;
     }
 
