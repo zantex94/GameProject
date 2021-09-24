081b28a1a9218e9beefe0193ffb431b8212077dd
andrewras93 <andrewras93@gmail.com>
2021-09-24 09:32:21 +0200

    Changed barroev html name.

--- javascript/main.js
+++ javascript/main.js
@@ -23,7 +23,7 @@ l5.appendChild(a5);
 
 l1.setAttribute("class", "navigationList");
 a1.setAttribute("class", "navigationText");
-a1.setAttribute("href", "barreov.html");
+a1.setAttribute("href", "barroev.html");
 a1.innerHTML = "Barroev";
 
 
